af9419b15949f48975840bb14162d05cb05e6fe3
James Bajamundi <jamesbjmnd@gmail.com>
2019-12-18 03:41:32 -0800

    fully functioning trivia game implemented

--- jamesbot.js
+++ jamesbot.js
@@ -18,6 +18,8 @@ app.listen(PORT, () => {
     console.log('Server started on port 3000...');
 });
 
+app.use(express.urlencoded({ extended: true }))
+
 const bot = new SlackBot({
     token: `${process.env.BOT_TOKEN}`,
     name: 'jamesbot'
@@ -62,7 +64,7 @@ bot.on('message', data => {
 // Response to Data
 // Use "!" prefix to post in #general channel
 function handleMessage(message) {
-    var message = message.split(' ')
+    var message = message.split(' ');
 
     switch (message[0]) {
         case '!yomama':
@@ -71,12 +73,101 @@ function handleMessage(message) {
         case '!ping':
             bot.postMessageToChannel('general', 'pong!');
             break;
-        case '!trivia':
+        case '!trivia2':
             triviaGameAny();
             break;
-        case '!trivia2':
-            triviaGame2();
+        case '!trivia':
+            bot.postMessageToChannel('general', 'Please choose a category: \nGeneral Knowledge[9], Books[10], Film[11], Music[12], Musicals[13], Television[14], Video Games[15], Board Games[16], Science & Nature[17], Science: Computers[18], Science: Mathematics[19], Mythology[20], Sports[21], Geography[22], History[23], Politics[24], Art[25], Celebrities[26], Animals[27], Vehicles[28], Comics[29], Gadgets[30], Anime[31], Cartoons[32] \nType !c [#]');
             break;
+        case '!c':
+            message.shift()
+            axios.get('https://opentdb.com/api.php?amount=2&category=' + message).then(response => {
+
+                let questionNumber = 1;
+                let questionIndex = 0;
+                let points = 0;
+
+                renderQuestion();
+
+                function renderQuestion() {
+                    if (questionIndex < response.data.results.length) {
+                        let answers = [
+                            response.data.results[questionIndex].correct_answer,
+                            response.data.results[questionIndex].incorrect_answers[0],
+                            response.data.results[questionIndex].incorrect_answers[1],
+                            response.data.results[questionIndex].incorrect_answers[2],
+                        ]
+
+                        let answerShuffle = function (arr) {
+                            let newPos, temp;
+                            for (let i = arr.length - 1; i > 0; i--) {
+                                newPos = Math.floor(Math.random() * (i + 1));
+                                temp = arr[i];
+                                arr[i] = arr[newPos];
+                                arr[newPos] = temp;
+                            }
+                            return arr;
+                        };
+
+                        let newArray = answerShuffle(answers);
+
+                        let shuffledAnswers = newArray.join(", ");
+
+                        let triviaQuestion = [
+                            `Question ${questionNumber}: ` + response.data.results[questionIndex].question,
+                            'Choices: ' + shuffledAnswers
+                        ].join('\n\n');
+
+                        bot.postMessageToChannel('general', `${triviaQuestion}`)
+
+
+                        console.log(response.data.results[questionIndex].correct_answer);
+                        console.log(message)
+                        bot.on('message', data => {
+                            if (data.type !== 'message') {
+                                return;
+                            }
+
+                            handleTrivia(data.text);
+                        });
+
+                        function handleTrivia(userInput) {
+
+                            if (userInput.toUpperCase() === response.data.results[questionIndex].correct_answer.toUpperCase()) {
+                                console.log('hi')
+                                points++;
+                                questionIndex++;
+                                questionNumber++;
+
+                                renderQuestion();
+                            } else if (userInput.toUpperCase() === response.data.results[questionIndex].incorrect_answers[0].toUpperCase() || userInput === response.data.results[questionIndex].incorrect_answers[1].toUpperCase() || userInput === response.data.results[questionIndex].incorrect_answers[2].toUpperCase()) {
+                                bot.postMessageToChannel('general', `Incorrect! It was ${response.data.results[questionIndex].correct_answer}`);
+                                questionIndex++;
+                                questionNumber++;
+                                renderQuestion();
+                            }
+                        }
+                    } else {
+                        bot.postMessageToChannel('general', `Score: ${points}/${questionIndex}`)
+                        points = 0;
+                        questionIndex = 0;
+                        questionNumber = 1;
+                        for (var i = 0; i < 5; i++) {
+                            bot.removeAllListeners('message', handleTrivia);
+                        }
+                        bot.on('message', data => {
+                            if (data.type !== 'message') {
+                                return;
+                            }
+
+                            handleMessage(data.text);
+                        });
+                    }
+                }
+            })
+            break;
+        case '!t':
+
         case '!delete':
             clear.clear();
             break;
@@ -84,7 +175,7 @@ function handleMessage(message) {
             app.listen(8000);
             break;
         case '!help':
-            bot.postMessageToChannel('general', 'Commands List: \n!yomama \n!ping \n!trivia \n!trivia2 \n!giphy [input]');
+            bot.postMessageToChannel('general', 'Commands List: \n!yomama \n!ping \n!trivia \n!trivia2 (currently in development) \n!giphy [input]');
             break;
         case `!giphy`:
             message.shift()
@@ -102,13 +193,9 @@ function handleMessage(message) {
             });
 
             break;
-        default: {
-            if (message[0][0] === '!') {
-                bot.postMessageToChannel('general', "sorry, that's not a command! :disappointed:");
-            }
-        }
 
     };
+
 
 };
 
@@ -196,9 +283,4 @@ function triviaGameAny() {
 
 }
 
-function triviaGame2() {
-    bot.postMessageToChannel('general', 'Please choose a category: \nGeneral Knowledge, Books, Film, Music, Musicals, Television, Video Games, Board Games, Science & Nature, Science: Computers, Science: Mathematics, Mythology, Sports, Geography, History, Politics, Art, Celebrities, Animals, Vehicles, Comics, Gadgets, Anime, Cartoons');
 
-
-}
-// Delete Message
